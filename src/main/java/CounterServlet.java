754f44d76323e00b008c17334c201987e2eb4320
Johnny Wells <johnnyw01github@gmail.com>
2023-03-08 15:26:28 -0600

    Recommiting work.

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -10,6 +10,10 @@ public class CounterServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         counter += 1;
-        response.getWriter().println("<h1>The count is " + counter + ".</h1>");
+        if(counter == 1){
+            response.getWriter().println("<h1>You've visited this page " + counter + " time!</h1>");
+        }else{
+            response.getWriter().println("<h1>You've visited this page " + counter + " times!</h1>");
+        }
     }
 }
