8a912ba2f1e0959a6095106e2edbcf4d273965d6
Johnny_W <101821626+johnnyw01@users.noreply.github.com>
2023-03-15 16:39:13 -0500

    Update DaoFactory.java

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -2,11 +2,10 @@ import java.sql.SQLException;
 
 public class DaoFactory {
     private static Ads adsDao;
-    private static Config config = new Config("jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false", "root", "codeup");
 
     public static Ads getAdsDao() throws SQLException {
         if (adsDao == null) {
-            adsDao = new MySQLAdsDao(config);
+            adsDao = new MySQLAdsDao(new Config());
 
         }
         return adsDao;
